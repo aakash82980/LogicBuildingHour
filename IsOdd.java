bb4bdb3cee829d2d3545dfb23f32a6702497a4db
Akash Sahoo <73008598+aakash82980@users.noreply.github.com>
2022-05-09 22:35:55 +0530

    Create IsOdd.java

---
+++ IsOdd.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+// Read only region start
+
+class UserMainCode
+{
+  public int isOdd(int input1){
+    //  Read only region end if(input1%2==0)
+    if(input1%2==0)
+		return 1;
+		return 2;
+		//throw new UnsupportedOperationException("isEven(int input1)");
+  }
+}
