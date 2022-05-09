11964c6d0fa7e4a16ea4760ec46c5bc5469ce64c
Akash Sahoo <73008598+aakash82980@users.noreply.github.com>
2022-05-09 22:44:46 +0530

    Create last_digit.java

---
+++ last_digit.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+// Read only region start
+
+class UserMainCode
+{
+  public int lastDigitOf(int input1){
+    //  Read only region end 
+    return Math.abs(input1)%10;
+	}
+}
