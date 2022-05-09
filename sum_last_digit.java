2c65d2cc29e8da6a96569767217b5ddd3f2d4c28
Akash Sahoo <73008598+aakash82980@users.noreply.github.com>
2022-05-09 22:53:04 +0530

    Create sum_last_digit.java

---
+++ sum_last_digit.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+// Read only region start
+
+class UserMainCode
+{
+  public int addLastDigits(int input1,int input2){
+    //  Read only region end 
+    return Math.abs(input1)%10+Math.abs(input2)%10;
+		//throw new UnsupportedOperationException("addLastDigits(int input1,int input2)");
+	}
+}
