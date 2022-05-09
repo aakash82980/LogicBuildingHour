ae2c9ac9211735bce9a6799eba62217f744fbf9a
Akash Sahoo <73008598+aakash82980@users.noreply.github.com>
2022-05-09 22:31:34 +0530

    Create Is Even.java

---
+++ Is Even.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+// Read only region start
+
+class UserMainCode
+{
+  public int isEven(int input1){
+    //  Read only region end if(input1%2==0)
+    if(input1%2==0)
+		return 2;
+		return 1;
+		//throw new UnsupportedOperationException("isEven(int input1)");
+  }
+}
